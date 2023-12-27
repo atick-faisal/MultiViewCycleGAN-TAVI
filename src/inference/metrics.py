f2b49f1bfc109b0db5bbc8a8fd213d3f0f6e414a
Atick Faisal <atickfaisal@gmail.com>
2023-12-27 18:22:29 +0300

    Update metrics

--- src/inference/metrics.py
+++ src/inference/metrics.py
@@ -19,11 +19,11 @@ def calculate_metrics(image_folder):
             if os.path.exists(fake_image_path):
                 real_image = cv2.imread(real_image_path)
                 fake_image = cv2.imread(fake_image_path)
-                real_image = cv2.resize(real_image, (256, 256))
+                real_image = cv2.resize(real_image, (512, 512))
 
                 # Ensure the images are the same size
                 if real_image.shape == fake_image.shape:
-                    mse = np.mean((real_image - fake_image) ** 2)
+                    mse = np.mean((real_image[:, :, 2] - fake_image[:, :, 2]) ** 2)
                     rmse = math.sqrt(mse)
                     # ssim_value = ssim(real_image, fake_image)
 
@@ -38,7 +38,7 @@ def calculate_metrics(image_folder):
     return avg_mse, avg_rmse, 0
 
 # Usage
-image_folder = '/home/ai/Downloads/CG_Nov-27-11_59AM-20231127T130336Z-001/CG_Nov-27-11_59AM/'
+image_folder = '/home/ai/Downloads/results/tavi_512_unaligned/test_latest/images/'
 avg_mse, avg_rmse, avg_ssim = calculate_metrics(image_folder)
 print(f"Average MSE: {avg_mse}, Average RMSE: {avg_rmse}, Average SSIM: {avg_ssim}")
 
@@ -53,7 +53,15 @@ print(f"Average MSE: {avg_mse}, Average RMSE: {avg_rmse}, Average SSIM: {avg_ssi
 # Average MSE: 16.839275723411923, Average RMSE: 4.097746027980363, Average SSIM: 0.8962748288778994
 # Average MSE: 14.03707988678463, Average RMSE: 3.7412332685817997, Average SSIM: 0
 
+# B: Average MSE: 11.08862640744164, Average RMSE: 3.323801479510094
+# G: Average MSE: 16.77762876238142, Average RMSE: 4.090425049072218
+# R: Average MSE: 14.24498449053083, Average RMSE: 3.76856104823703
+
 # UNet
 # Average MSE: 13.77025690532866, Average RMSE: 3.6943809085779313, Average SSIM: 0.9308193108223433
 # Average MSE: 12.31149028596424, Average RMSE: 3.492069471009254
 # Average MSE: 12.18688503901164, Average RMSE: 3.471797798906307
+
+# B: Average MSE: 7.5183186531066895, Average RMSE: 2.719488296938165
+# G: Average MSE: 14.2801144917806, Average RMSE: 3.7586947206473567
+# R: Average MSE: 14.762221972147623, Average RMSE: 3.8239731353886177
